94cff9a35c797b8da41b91f17431754e430e9f97
R4ttlesnake <alexniyuchen@hotmail.com>
2019-10-13 17:54:25 -0700

    Update Command_maxbuff.java
    Test to confirm functionality of git.

--- src/ca/bc/southridge/ccc/mmo/commands/Command_maxbuff.java
+++ src/ca/bc/southridge/ccc/mmo/commands/Command_maxbuff.java
@@ -16,6 +16,7 @@ public class Command_maxbuff implements CommandExecutor {
 		}
 		Player player = (Player) arg0;
 		player.getAttribute(Attribute.GENERIC_MAX_HEALTH).addModifier(new AttributeModifier("Increase vitality.", 1, AttributeModifier.Operation.MULTIPLY_SCALAR_1));
+		player.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).addModifier(new AttributeModifier("Increase speed.", 1, AttributeModifier.Operation.MULTIPLY_SCALAR_1));
 		return true;
 	}
 
