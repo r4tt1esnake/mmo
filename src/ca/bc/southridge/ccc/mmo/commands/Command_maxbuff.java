0b30170aff43b1cde8d5a6c8f654a2ef3a410f4c
taizhang22 <41173438+taizhang22@users.noreply.github.com>
2019-11-03 15:48:35 -0800

    Update Command_maxbuff.java
    testing this

--- src/ca/bc/southridge/ccc/mmo/commands/Command_maxbuff.java
+++ src/ca/bc/southridge/ccc/mmo/commands/Command_maxbuff.java
@@ -7,6 +7,7 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+//Hopefully this works
 public class Command_maxbuff implements CommandExecutor {
 
 	@Override
